9871b1a9d9c063c64133d3953d94a12f2abeed19
yumeart2467 <144347907+yumeart2467@users.noreply.github.com>
2023-09-08 08:52:18 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$('#slide').owlCarousel({
+  loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+  autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,
+	dots:true,// cho xuất hiện bộ nút tới lui 
+})
